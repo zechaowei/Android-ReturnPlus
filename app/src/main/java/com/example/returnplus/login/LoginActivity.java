6eb83c6b046a25f697f671e98f02267ff4c97fca
zechaowei <zechaowei2001@gmail.com>
2023-05-11 01:57:15 +0800

    v1.1 美化登录界面|设置自动登录和忘记密码

--- app/src/main/java/com/example/returnplus/login/LoginActivity.java
+++ app/src/main/java/com/example/returnplus/login/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.returnplus.login;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,9 @@ import com.example.returnplus.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView tvForgetPassword;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,16 @@ public class LoginActivity extends AppCompatActivity {
         //去掉标题行
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        //找到忘记密码这个按钮
+        tvForgetPassword = findViewById(R.id.tvForgetPassword);
+
+
+        //设置布局文件
         setContentView(R.layout.activity_login);
+
+
+//        //设置点击忘记密码时跳转到对应网站，例如http://www.baidu.coms
+//        tvForgetPassword.setText(Html.fromHtml("<a href=\"http://www.baidu.com\">忘记密码？</a>"));
+//        tvForgetPassword.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
