63923f31ef38fd6d4b16c4c1bdf10d144c9224ff
zechaowei <zechaowei2001@gmail.com>
2023-05-21 17:44:59 +0800

    v1.8.0 实现日记界面以及部分优化

--- app/src/main/java/com/example/returnplus/DiaryActivity.java
+++ app/src/main/java/com/example/returnplus/DiaryActivity.java
@@ -2,13 +2,23 @@ package com.example.returnplus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.returnplus.diary.Diary1Activity;
+
 public class DiaryActivity extends AppCompatActivity {
+
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private ImageView imageView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,21 @@ public class DiaryActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);    //实际上是在菜单项目上显示菜单（在左边显示）
 
         setContentView(R.layout.activity_diary);
+
+        //实现日记页面
+        imageView1 = findViewById(R.id.diary_1);
+        imageView2 = findViewById(R.id.diary_2);
+        imageView3 = findViewById(R.id.diary_3);
+
+        //跳转到第一个日记界面
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DiaryActivity.this, Diary1Activity.class);
+                Toast.makeText(DiaryActivity.this,"跳转到小确幸界面",Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
     }
 
     /**
@@ -35,6 +60,15 @@ public class DiaryActivity extends AppCompatActivity {
             Intent intent = new Intent(DiaryActivity.this, MainActivity.class);
             startActivity(intent);
         }
+        return true;
+    }
+
+
+    //实现 新增日记按钮
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //将资源文件(xml形式的菜单内容)渲染到menu中
+        getMenuInflater().inflate(R.menu.add_diary,menu);
         return true;
     }
 }
---
+++ app/src/main/java/com/example/returnplus/diary/Diary2Activity.java
@@ -0,0 +1,16 @@
+package com.example.returnplus.diary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.returnplus.R;
+
+public class Diary2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary2);
+    }
+}
