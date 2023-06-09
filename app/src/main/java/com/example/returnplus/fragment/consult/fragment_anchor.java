6f045e3ca480bc9abac8f05aaae7a1e2770d60df
zechaowei <zechaowei@MacBook-Pro.local>
2023-06-09 19:37:04 +0800

    v2.1.1实现主播页面

---
+++ app/src/main/java/com/example/returnplus/fragment/consult/fragment_anchor.java
@@ -0,0 +1,27 @@
+package com.example.returnplus.fragment.consult;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.returnplus.R;
+
+
+/**
+ * 主播页面
+ */
+public class fragment_anchor extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_anchor, container, false);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/returnplus/fragment/consult/fragment_video.java
@@ -0,0 +1,27 @@
+package com.example.returnplus.fragment.consult;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.returnplus.R;
+
+
+/**
+ * 视频页面
+ */
+public class fragment_video extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_video, container, false);
+
+
+        return view;
+    }
+}
